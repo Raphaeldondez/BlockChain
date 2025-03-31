0cf7c8f40e9c6a84e577932c4433a89965edb7a6
raphael dondez <raphael59219@hotmail.com>
2025-03-31 18:56:48 +0200

    signature

--- block_chain.py
+++ block_chain.py
@@ -1,8 +1,12 @@
 #coding utf-8
 
 import hashlib
-import rsa
+import cryptography
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import hashes
 
+
+#import Web3, EthereumTesterProvider
 
 #Définition des structures
 class block :
@@ -32,14 +36,16 @@ class blockchain:
 
 class user :
     name : str
-    public_key : str
-    private_key : str
+    public_key : cryptography.hazmat.primitives.asymmetric.rsa.RSAPublicKey
+    private_key : cryptography.hazmat.primitives.asymmetric.rsa.RSAPrivateKey
+    wallet : int
 
 class transactions :
+    id_transaction : str
     id_sender : str
     id_receiver : str
     amount : str
-    signature :str
+    nonce :str
 
 
 #Génération des blocs
@@ -105,18 +111,19 @@ def print_blockchain(bc: blockchain):
 
 
 
-
-    
-#Gestion des utilisateur
-def create_user (name):
+#Gestion des utilisateur :
+def create_user (name, wallet):
     new = user()
     new.name = name
-    (pubkey, privkey) = rsa.newkeys(1024)
+    new.wallet = wallet
+    privkey = rsa.generate_private_key(public_exponent=65537, key_size=2048 )
     new.private_key = privkey
-    new.public_key = pubkey
+    new.public_key = privkey.public_key()
     return new
 
+
 def display_user(user : user):
+    print("User: ", user.name)
     print("User: ", user.name)
     print("Public key: ", user.public_key)
     print("Private key: ", user.private_key)
@@ -124,17 +131,35 @@ def display_user(user : user):
 
 #Gestion des transactions :
 
-def create_transactions (receiver, sender, amount):
+def create_transactions (id_transaction, receiver: user, sender: user, amount):
     new = transactions()
+    new.id_transaction = id_transaction
     new.id_receiver = receiver
     new.id_sender = sender
     new.amount = amount
+    message = b"HELLO"
+    new.nonce = sender.private_key.sign( message, padding.PSS( mgf=padding.MGF1(hashes.SHA256()), salt_length=padding.PSS.MAX_LENGTH), hashes.SHA256() )
+   
+    return new
 
-    new.signature
+def display_transaction ( transaction :transactions):
+    print("Transaction id : ", transaction.id_transaction)
+    print("From :", transaction.id_sender)
+    print("To: ", transaction.id_receiver)
+    print("Amount: ", transaction.amount)
+    print("Signature: ", transaction.nonce)
+
+
+
+
+
+
+
+
 
 #Main
 print("Beginning\n\n")
-
+'''
 bc = create_blockchain("Victory", 4)
 mining(bc, "Bonjour")
 mining(bc, "Bonjour")
@@ -142,7 +167,12 @@ mining(bc, "Bonjour")
 print_blockchain(bc)
 '''
 
-j1 = create_user("GOAT")
-display_user(j1)
-'''
+j1 = create_user("Maya", 60)
+j2 = create_user("Léon", 10)
+#display_user(j1)
+#display_user(j2)
+
+trans_1 = create_transactions("trans_1", j1, j2, 50)
+display_transaction(trans_1)
+
 print("\n\nEnding")
